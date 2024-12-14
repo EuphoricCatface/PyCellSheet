96b58b355795cab70b79b8fb5b2534daa078c9b5
Seongyong Park <EuphoricCatface@gmail.com>
2024-12-14 15:52:11 +0900

    spreadsheet.logical implemented, save for LAMBDA() and LET()

---
+++ pyspread/lib/spreadsheet/logical.py
@@ -0,0 +1,82 @@
+try:
+    from pyspread.lib.custom_classes import EmptyCell
+    from pyspread.lib.spreadsheet.errors import SpreadsheetErrorNa
+except ImportError:
+    from lib.custom_classes import EmptyCell
+
+
+def AND(*args):
+    return all(args)
+
+
+def FALSE():
+    return False
+
+
+def IF(expr, true, false):
+    return true if expr else false
+
+
+def IFERROR(value, value_if_error=EmptyCell):
+    return value_if_error if isinstance(value, BaseException) else value
+
+
+def IFNA(value, value_if_na):
+    if isinstance(value, SpreadsheetErrorNa):
+        return value_if_na
+    return value
+
+
+def IFS(*args):
+    matched = False
+    for i, arg in enumerate(args):
+        if (i % 2) == 0:
+            # arg is condition
+            matched = bool(arg)
+            continue
+        if matched:
+            return arg
+    return SpreadsheetErrorNa()
+
+
+def LAMBDA(name, formula_expression):
+    raise NotImplemented("LAMBDA() not implemented yet. Use Python's native lambda.")
+
+
+def LET(a, b):
+    raise NotImplemented("LET() not implemented yet. Do you really need this function, when you have python?")
+
+
+def NOT(expr):
+    return not expr
+
+
+def OR(a, b):
+    return a or b
+
+
+def SWITCH(expr, *args):
+    default = SpreadsheetErrorNa()
+    if len(args) % 2:
+        default = args[-1]
+        args = args[:-1]
+
+    matched = False
+    for i, arg in enumerate(args):
+        if (i % 2) == 0:
+            # arg is condition
+            if expr == arg:
+                matched = True
+            continue
+        if matched:
+            return arg
+    return default
+
+
+
+def TRUE():
+    return True
+
+
+def XOR(args):
+    return len(list(filter(bool, args))) % 2
