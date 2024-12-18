00be7673ededb4e961ca18dae3f5f3ee2584a313
Seongyong Park <EuphoricCatface@gmail.com>
2024-12-19 01:05:18 +0900

    pycellsheet: update Range

--- pyspread/lib/pycellsheet.py
+++ pyspread/lib/pycellsheet.py
@@ -30,11 +30,20 @@ class HelpText:
         self.contents = contents
 
 
-class Range(list):
-    def __init__(self):
-        super().__init__()
-        self.top_left = None
+class Range:
+    def __init__(self, topleft: str, lst: list, width: int):
+        self.topleft = topleft
+        self.lst = lst
+        self.width = width
+
+        if len(self.lst) % width:
+            raise ValueError("Length of the list is not divisible with the width")
 
     def flatten(self) -> list:
-        self: list[list[Any]]
-        return [elem for inner_lst in self for elem in inner_lst]
+        return self.lst
+
+    def __getitem__(self, item: int):
+        if item >= len(self.lst) / self.width:
+            raise IndexError("Index out of range")
+        return self.lst[self.width * item:self.width * (item + 1)]
+
