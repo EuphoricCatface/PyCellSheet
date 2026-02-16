08065e217fc6b7b75fddbd0ffd9729eed70f077b
Seongyong Park <EuphoricCatface@gmail.com>
2026-02-16 12:08:06 +0900

    pycs: normalize sheet names before writing sections

--- pyspread/lib/test/test_pycs_sheet_names.py
+++ pyspread/lib/test/test_pycs_sheet_names.py
@@ -24,6 +24,14 @@ class _DummyCodeArray:
         self.cell_attributes = []
 
 
+class _DummyWriterCodeArray:
+    def __init__(self, sheet_names, macros):
+        self.shape = (1, 1, len(macros))
+        self.dict_grid = _DummyDictGrid()
+        self.dict_grid.sheet_names = sheet_names
+        self.dict_grid.macros = macros
+
+
 def test_pycs2sheet_names_sanitizes_and_uniquifies():
     code_array = _DummyCodeArray(3)
     reader = PycsReader(BytesIO(b""), code_array)
@@ -43,3 +51,17 @@ def test_finalize_sheet_names_fills_missing_defaults():
     reader._finalize_sheet_names()
 
     assert code_array.dict_grid.sheet_names == ["Only One", "Sheet 1", "Sheet 2"]
+
+
+def test_writer_normalizes_sheet_names_for_output():
+    sys.path.insert(0, pyspread_path)
+    from interfaces.pycs import PycsWriter
+    sys.path.pop(0)
+
+    code_array = _DummyWriterCodeArray(
+        ["Good", " ", "Bad\tName", "Good"],
+        ["a=1", "b=2", "c=3", "d=4"],
+    )
+    writer = PycsWriter(code_array)
+
+    assert writer._normalized_sheet_names() == ["Good", "Sheet 1", "BadName", "Good_1"]
